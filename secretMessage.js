a82569d8f254187161c23cf82d82c9a51779027b
loganed09 <64222358+loganed09@users.noreply.github.com>
2021-04-05 16:38:34 -0400

    Create secretMessage.js

---
+++ secretMessage.js
@@ -0,0 +1,21 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+
+const arrayLength = secretMessage.length;
+console.log(arrayLength);
+
+secretMessage.push('to','Program');
+
+secretMessage[7] = 'right';
+
+secretMessage.shift();
+
+secretMessage.unshift('Programming');
+
+secretMessage.splice(6,11,'know');
+
+
+
+console.log(secretMessage.join(' '));
